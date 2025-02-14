acfcff6d605f45c9b64be5b1fb068a69e938ebba
bradc <brad.cocar@gmail.com>
2025-02-14 11:17:20 -0800

    Remove chat messages at start of game.

--- src/ChatBox.java
+++ src/ChatBox.java
@@ -16,10 +16,7 @@ public class ChatBox extends GridPane {
         getRowConstraints().add(new RowConstraints(60));
         setStyle("-fx-background-color: lightgray");
         setPadding(new Insets(0, 0, 0, 8));
-        chatMessages.add(new ChatMessage("Enemy has sunk your battleship. That's too bad lol."));
-        chatMessages.add(new ChatMessage("You shoot at 1,1. Miss!"));
-        chatMessages.add(new ChatMessage("Enemy shoots at 1,5. Miss!"));
-        chatMessages.add(new ChatMessage("You have hit the enemy's destroyer! Are you secretly a pro?"));
+        chatMessages.add(new ChatMessage("Welcome to Battleship."));
         updateChatMessages();
     }
 
