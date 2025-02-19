0f093a3ceea66d3d05fbf5e6b08baff1dd593868
bradc <brad.cocar@gmail.com>
2025-02-19 15:30:27 -0800

    Added replay label to show whose turn it is.

--- src/Replay.java
+++ src/Replay.java
@@ -1,10 +1,15 @@
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 import java.util.ArrayList;
@@ -14,20 +19,47 @@ public class Replay extends Scene {
     private MatchOutcome outcome;
     private ReplayBoard playerReplayBoard;
     private ReplayBoard enemyReplayBoard;
+    private Text turnPlayer;
+    private Text turnLabel;
+    private final String userTextColor = "#039e03";
+    private final String enemyTextColor = "#d92e2e";
 
     public Replay(MatchOutcome outcome) {
         this(new VBox());
         this.outcome = outcome;
         playerReplayBoard = new ReplayBoard();
         enemyReplayBoard = new ReplayBoard();
-        mainPane.setSpacing(30);
-        mainPane.getChildren().addAll(enemyReplayBoard, playerReplayBoard);
+        Text turnText = new Text("Turn: ");
+        turnText.setFont(Font.font("Elephant", FontWeight.BOLD, 18));
+        turnLabel = turnText;
+        Text turnPlayer = new Text("User");
+        this.turnPlayer = turnPlayer;
+        turnPlayer.setFont(Font.font("Elephant", FontWeight.BOLD, 18));
+        turnPlayer.setStyle("-fx-fill: " + userTextColor + ";");
+        HBox turnTextContainer = new HBox();
+        turnTextContainer.getChildren().addAll(turnText, turnPlayer);
+        turnTextContainer.setAlignment(Pos.CENTER);
+        mainPane.getChildren().addAll(enemyReplayBoard, turnTextContainer, playerReplayBoard);
         showReplay();
     }
 
     public Replay(VBox mainPane) {
         super(mainPane);
         this.mainPane = mainPane;
+    }
+
+    private void setTurnPlayerText(String player) {
+        if (player.equals("user")) {
+            turnPlayer.setStyle("-fx-fill: " + userTextColor + ";");
+            turnPlayer.setText("User");
+        } else if (player.equals("enemy")) {
+            turnPlayer.setStyle("-fx-fill: " + enemyTextColor + ";");
+            turnPlayer.setText("Enemy");
+        } else {
+            turnPlayer.setStyle("-fx-fill: #0095ff;");
+            turnPlayer.setText("WINNER: " + outcome.getWinner());
+            turnLabel.setText("");
+        }
     }
 
     public void showReplay() {
@@ -40,10 +72,12 @@ public class Replay extends Scene {
                     Duration.seconds(0.5),
                     event -> {
                         if (turn[0].equals("user")) {
+                            setTurnPlayerText("user");
                             updateHits(enemyReplayBoard, i[0]);
                             i[0]++;
                             turn[0] = "enemy";
                         } else if (turn[0].equals("enemy")) {
+                            setTurnPlayerText("enemy");
                             updateHits(playerReplayBoard, j[0]);
                             j[0]++;
                             turn[0] = "user";
@@ -54,6 +88,7 @@ public class Replay extends Scene {
         timeline.setCycleCount(cycles);
         timeline.play();
         timeline.setOnFinished(e -> {
+            setTurnPlayerText("winner");
             Dialog<String> replayFinishedDialog = new Dialog<String>();
             replayFinishedDialog.setTitle("Replay Finished");
             ButtonType OK = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
